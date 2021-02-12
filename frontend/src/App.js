219a59cadc5c1bce27d465751a9290588b03f30e
Suyog Subedi <subedisuyog555@gmail.com>
2021-02-12 10:06:00 +0545

    Added products in home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import HomeScreen from "./Screens/HomeScreen";
 const App = () => {
   return (
     <div className="App">
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Nuestra Ecommerce</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
