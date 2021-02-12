18950ccec66ca417f31ed37213b13d7cb39438be
Suyog Subedi <subedisuyog555@gmail.com>
2021-02-12 09:03:00 +0545

    Header Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to Nuestra</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Nuestra Ecommerce</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
